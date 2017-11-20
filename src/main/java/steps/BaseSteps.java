4f3f865a43b9507072f00032d0ef23082d0aa94d
Натали <nati.serdyuk@gmail.com>
2017-11-20 21:30:20 +0300

    20.11

---
+++ src/main/java/steps/BaseSteps.java
@@ -0,0 +1,33 @@
+package steps;
+
+import net.thucydides.core.annotations.Managed;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Maria on 29.07.2017.
+ */
+
+public class BaseSteps {
+
+    @Managed(driver = "chrome")
+    WebDriver driver;
+
+    @Before
+    public void beforeMethod(){
+        System.setProperty ("webdriver.chrome.driver", "D:/Мои документы/Работа/АПЛАНА/JAVA/курсы в субботу/serenitydemo/drv/chromedriver.exe");
+        driver.get(System.getProperty("app.url"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void afterMethod(){
+        driver.quit();
+    }
+
+}
